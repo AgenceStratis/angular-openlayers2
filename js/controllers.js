e3aa72e17539015d73a1fccce5a3466d388a871a
Romain MOREAU <moreau.romain83@gmail.com>
2015-04-15 11:37:47 +0200

    add external menu

--- js/controllers.js
+++ js/controllers.js
@@ -2,48 +2,80 @@ angular.module("cartoControllers", ["cartoServices"])
     .controller("PageCtrl", function ($scope, $http) {
 
         $scope.newSource = true;
+        $scope.first = true;
 
-        $scope.changeSource = function(){
-            if($scope.newSource){
-                $http.get("data2.json").then(function(result){
+        $scope.changeSource = function () {
+            if ($scope.first) {
+                $scope.first = false;
+            } else {
+                $scope.close();
+            }
+
+            if ($scope.newSource) {
+                $http.get("data2.json").then(function (result) {
                     $scope.layers = result.data;
-                    $scope.currentLayer= result.data[0].name
+                    $scope.currentLayer = result.data[0].name
                 });
             } else {
                 $http.get("data.json").then(function (result) {
                     $scope.layers = result.data;
-                    $scope.currentLayer= result.data[0].name
+                    $scope.currentLayer = result.data[0].name
                 });
             }
             $scope.newSource = !$scope.newSource;
         };
-        $scope.changeSource();
+
 
         $scope.test = true;
 
-        $scope.toggle = function(){
-            $scope.test = !$scope.test;
+        $scope.toggle = function () {
+            var element = angular.element(document.getElementById("content"));
+            if (element.hasClass("show")) {
+                $scope.close();
+            } else {
+                $scope.open();
+            }
+        };
+
+        $scope.close = function () {
+            var element = angular.element(document.getElementById("content"));
+            if (element.hasClass("show")) {
+                element.removeClass("show");
+                element.addClass("hide");
+            }
+            setTimeout(1, function(){
+                $scope.currentDatas = "";
+            });
+
+        };
+
+        $scope.open = function () {
+            if($scope.currentDatas !== "") {
+                var element = angular.element(document.getElementById("content"));
+                element.addClass("show");
+                element.removeClass("hide");
+            }
         };
 
         $scope.currentLayer = "orange";
 
-        $scope.style = function(feature){
+        $scope.style = function (feature) {
             feature.style.label = feature.attributes.name;
-            feature.style.strokeColor="pink";
+            feature.style.strokeColor = "pink";
         };
 
-        $scope.onClick = function(olMarker, datas, markers){
+        $scope.currentDatas = "";
+
+        $scope.onClick = function (olMarker, datas, markers) {
             for (var marker in markers) {
                 angular.element(markers[marker].icon.imageDiv).removeClass("selected")
             }
             angular.element(olMarker.icon.imageDiv).addClass("selected");
-            popup = new OpenLayers.Popup("chicken",
-                olMarker.lonlat,
-                new OpenLayers.Size(200,200),
-                datas.name,
-                true);
-            olMarker.map.addPopup(popup, true);
+            $scope.currentDatas = datas;
+            $scope.$apply(); //Called from outside of Angular, so whe must explictly called $apply to update scope
+            $scope.open();
+        };
 
-        }
+        $scope.changeSource();
     })
 ;
